1f22ef35024ee04f7948bcb3b24d4a42fcfd53bb
user <user@vm-ubuntu-14.04>
2014-10-21 23:08:18 +0100

    working

--- My_WS1/serverThreaded.c
+++ My_WS1/serverThreaded.c
@@ -14,7 +14,7 @@
 #define CONCURRENT 10
 
 int active_filenames = 0;  // the number of threads currently performing encryption or decryption on a file
-char **filename_ptr[CONCURRENT] = {NULL};
+char* pfiles[CONCURRENT] = {NULL};
 int count = 0;
 pthread_mutex_t mut; /* the lock */
 
@@ -27,15 +27,14 @@ void error(char *msg)
 } //end error
 
 /* function to decide if we are able to proceed with work on that filename */
-int proceed (struct Rqst request) {
+int proceed (struct Rqst* prequest) {
 	int i;
-char *tmpPtr;
+	char* tmpPtr;
   	pthread_mutex_lock (&mut); /* lock exclusive access to array filename_ptr */
 
 	for (i=0; i<count; ++i){
-printf("%s\n", "am in loop in proceed");
 		//check if filename is in active list
-		if (strcmp(request.filename, *filename_ptr[i])) {
+		if (strcmp( prequest->filename, pfiles[i])) {
 			//filename found - we will not proceed
 			pthread_mutex_unlock (&mut); /* release the lock */
 			return 99;
@@ -45,10 +44,10 @@ printf("%s\n", "am in loop in proceed");
 	// filename not found in currently active list - add to list 	
 	++count;  //increase count
 
-printf("%s\n", request.filename);
-tmpPtr = request.filename;
+printf("%s\n", prequest->filename );
+	tmpPtr = prequest->filename;
 
-	filename_ptr[count] = tmpPtr;
+	pfiles[count] = tmpPtr;
 	pthread_mutex_unlock (&mut); /* release the lock */
 	return count;  //returns position where we just entered our filename
 
@@ -56,42 +55,40 @@ printf("%s\n", "am leaving proceed");
 } //end proceed
 
 /*function that does encryption work */
-int do_gpg(struct Rqst request){
+int do_gpg(struct Rqst* prequest){
 	char gpg_file[35];
 	char command[151];
 	int plainflag;
 	int cipherflag;
 
-
 	//check if plaintext exists
-	if (access(request.filename,F_OK) != -1){
+	if (access(prequest->filename,F_OK) != -1){
 		plainflag = 2;
 	} else plainflag = 0;
 
-
 	//check if ciphertext exists
 	memset(gpg_file, '\0', sizeof(gpg_file));
-	strcpy(gpg_file, request.filename);
+	strcpy(gpg_file, prequest->filename);
 	strcat(gpg_file, ".gpg");
 
  	if ( access(gpg_file,F_OK) != -1){
 		cipherflag = 4;
 	} else cipherflag = 0;	
-	printf("%i  %i   %i\n", request.op, plainflag, cipherflag);
+	printf("%i  %i   %i\n", prequest->op, plainflag, cipherflag);
 
 	//sort out which case we are in
-	switch (cipherflag + plainflag + request.op){
+	switch (cipherflag + plainflag + prequest->op){
 		case (0 + 2 + 0):
 		   	// prepare for encrypt
 	   		snprintf(command, sizeof(command), "echo %s | gpg --batch -c --passphrase-fd 0 %s \n" , 
-				request.passphrase, request.filename);
+				prequest->passphrase, prequest->filename);
 			break;
 		case (4 + 0 + 1):
 			// prepare for decrypt
  		   	snprintf(command, sizeof(command), "echo %s | gpg --batch -d --passphrase-fd 0 --output %s %s.gpg\n" ,
-				 request.passphrase, request.filename, request.filename);
+				 prequest->passphrase, prequest->filename, prequest->filename);
 			break;
-		default: return (cipherflag + plainflag + request.op + 16); //error so don't proceed - 16 added to show it is an error code
+		default: return (cipherflag + plainflag + prequest->op + 16); //error so don't proceed - 16 added to show it is an error code
 	}
 
 	// go ahead and execute command
@@ -115,10 +112,10 @@ void *processRequest (void *args) {
 	 error ("ERROR reading from socket");
 
 	//decide if we can operate on that filename 
-	tmp = proceed(request);
+	tmp = proceed(&request);
 	if (99 > tmp) {
 		//we have exclusive use of that filename - do work
-    		rc = do_gpg(request);
+    		rc = do_gpg(&request);
 		printf("Error code %i\n",rc);
 
 		//construct the response
@@ -146,10 +143,10 @@ void *processRequest (void *args) {
 		}
 
 		//update array to remove filename and release our hold on it
-		pthread_mutex_lock (&mut); 				/* lock exclusive access to array filename_ptr */
-		filename_ptr[tmp] = filename_ptr[count];	/* remove entry by overwriting with last entry */
-		--count; 								/* decrease count to move end marker */
-		pthread_mutex_unlock (&mut); 			/* release the lock */
+		pthread_mutex_lock (&mut); 	/* lock exclusive access to array filename_ptr */
+		pfiles[tmp] = pfiles[count];	/* remove entry by overwriting with last entry */
+		--count; 			/* decrease count to move end marker */
+		pthread_mutex_unlock (&mut); 	/* release the lock */
 	} else {
 		strcpy(response.msg,"File currently in use - operation terminated");
 	}
